28002c04b6d656de778f17afaa1e8ca253cab32f
Christopher <christopherdudasthomas@gmail.com>
2023-08-23 13:52:32 -0700

    delete stub test files

--- tests/testthat/test-setup.R
+++
@@ -1,9 +0,0 @@
-test_that("setup properly assigns client variables", {
-  cat(Sys.getenv("RSTUDIO"))
-
-
-  # setup_client()
-  #
-  # expect_equal(Sys.getenv("base_url"), "https://api.quant-aq.com/device-api")
-})
-
--- tests/testthat/test-wrappers.R
+++
@@ -1,5 +0,0 @@
-test_that("as.data.frame", {
-  expect_equal(2 * 2, 4)
-})
-
-
